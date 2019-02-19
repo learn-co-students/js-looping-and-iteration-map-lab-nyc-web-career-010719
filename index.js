8791affcac111d1678235d303e7e4d795beb8b2b
cvolpe12 <christopher.volpe12@gmail.com>
2019-02-18 19:16:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+  return array.map(function(element) {
+    return element.toLowerCase();
+  });
+}
+
+function nameToAttributes(array) {
+  return array.map(function(element){
+    const fName = element.split(' ')[0];
+    const lName = element.split(' ')[1];
+    return {firstName: fName, lastName: lName};
+  });
+}
+
+function attributesToPhrase(array) {
+  return array.map(function(element){
+    return `${element[`name`]} is from ${element[`hometown`]}`;
+  });
+}
